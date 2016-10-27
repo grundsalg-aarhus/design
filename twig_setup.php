e26d15882540dc48fe66c4bad1b752d423b850f3
turegjorup <ture@ignatz.dk>
2016-10-27 10:47:37 +0200

    Fixed path

--- twig_setup.php
+++ twig_setup.php
@@ -8,7 +8,7 @@ Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem($doc_root.'/templates');
 $twig = new Twig_Environment($loader, array(
-  'cache' => $doc_root.'/compilation_cache',
+  'cache' => $doc_root.'compilation_cache',
   'debug' => true
 ));
 $twig->addExtension(new Twig_Extension_Debug());
